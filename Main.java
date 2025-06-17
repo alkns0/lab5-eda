dca4df1c3f815133baec9e047fae10b880402e4c
alkns0 <alkonsoxdf@gmail.com>
2025-06-17 14:01:55 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 import java.util.TreeMap;
-
+import java.util.ArrayList;
 
 class Player {
 	String playerName;
@@ -57,16 +57,17 @@ class ScoreBoard {
 			p1.addDraw();
 			p2.addLoss();
 		}
+
 	}
 	
 	public void registerPlayer(String playerName){
-	        if (!checkPlayer(playerName)){
-	            Player player = new Player(playerName);
-	            players.put(playerName, player);
-	        }	 
-	        else {
-	            System.out.println("Ya está en el tree map");
-	        }
+        if (!checkPlayer(playerName)){
+            Player player = new Player(playerName);
+            players.put(playerName, player);
+        }	 
+        else {
+            System.out.println("Ya está en el tree map");
+        }
 	}
 	
 	public boolean checkPlayer(String playerName){
@@ -78,10 +79,76 @@ class ScoreBoard {
 	    }
 	}
 	
+	public ArrayList <Player> winRange(int desde, int hasta){
+	    ArrayList <Player> lista = new ArrayList <Player>();
+	    return lista;
+	}
+	
+	
+	
 }
+
+class ConnectFour {
+    char[][] grid = new char[7][6];
+    char currentSymbol;
+    
+    public ConnectFour(){ 
+        for(int i = 0; i < 7; i++){
+            for(int j = 0; j < 6; j++){
+                grid[i][j] = ' ';
+            }
+        }
+        currentSymbol = 'X';
+    }
+    
+    public boolean makeMove(int z){
+        
+        if (grid[z][0] != ' ' || z > 7 || z < 0) { //Verificar si el primero está ocupado (Significa que esta lleno)
+            return false;
+        }
+        else { //Se puede realizar el movimiento
+            for(int k = 0; k < 6; k++){
+                if (grid[z][k] == ' '){
+                    grid[z][k] = currentSymbol;
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    
+    public boolean isGameOver(){
+        //Pendiente
+        return false;
+    }
+    
+}
+
+class Game {
+    String status;
+    String winnerPlayerName;
+    String playerNameA;
+    String playerNameB;
+    ConnectFour juego;
+    
+    public Game(String _playerNameA, String _playerNameB){
+        playerNameA = _playerNameA;
+        playerNameB = _playerNameB;
+        winnerPlayerName = "";
+        status = "IN_PROGRESS";
+        juego = new ConnectFour();
+    }
+    
+    public String play(){
+        return "";
+    }
+    
+}
+
+
 	public class Main
 	{
 		public static void main(String[] args) {
-            		System.out.println("holamundo");
+            System.out.println("holamundo");
 		}
 	}
